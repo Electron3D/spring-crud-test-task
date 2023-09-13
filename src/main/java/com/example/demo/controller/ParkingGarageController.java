7d1aaecd33aeda103469a70074cd28a977e39eda
Artem Prokhorov <electron3djavadev@gmail.com>
2023-09-13 11:12:50 +0300

    parking garage classes implemented, need to check performance

--- src/main/java/com/example/demo/controller/ParkingGarageController.java
+++ src/main/java/com/example/demo/controller/ParkingGarageController.java
@@ -1,11 +1,47 @@
 package com.example.demo.controller;
 
+import com.example.demo.data.RestResponse;
+import com.example.demo.data.dto.CarDto;
+import com.example.demo.data.dto.ParkingGarageDto;
+import com.example.demo.service.ParkingGarageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/garages")
 @RequiredArgsConstructor
 public class ParkingGarageController {
+    private final ParkingGarageService parkingGarageService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RestResponse createParkingGarage(@RequestBody ParkingGarageDto parkingGarageDto) {
+        parkingGarageService.create(parkingGarageDto);
+        return new RestResponse("Parking garage created");
+    }
+
+    @GetMapping("/{id}")
+    public ParkingGarageDto getParkingGarageById(@PathVariable Long id) {
+        return parkingGarageService.findById(id);
+    }
+
+    @GetMapping
+    public List<ParkingGarageDto> getAllParkingGarages() {
+        return parkingGarageService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public RestResponse updateParkingGarageById(@PathVariable Long id, @RequestBody ParkingGarageDto parkingGarageDto) {
+        parkingGarageService.updateById(id, parkingGarageDto);
+        return new RestResponse("Parking garage with id: " + id + " updated.");
+    }
+
+    @DeleteMapping("/{id}")
+    public RestResponse deleteParkingGarageById(@PathVariable Long id) {
+        parkingGarageService.deleteById(id);
+        return new RestResponse("Parking garage with id: " + id + " deleted");
+    }
 }
--- src/main/java/com/example/demo/service/impl/ParkingGarageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ParkingGarageServiceImpl.java
@@ -1,43 +1,70 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.data.dto.ParkingGarageDto;
+import com.example.demo.data.entity.Car;
+import com.example.demo.data.entity.ParkingGarage;
+import com.example.demo.data.entity.ParkingSlot;
+import com.example.demo.data.mapper.impl.ParkingGarageMapper;
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.repository.ParkingGarageRepository;
 import com.example.demo.service.ParkingGarageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ParkingGarageServiceImpl implements ParkingGarageService {
     private final ParkingGarageRepository parkingGarageRepository;
+    private final ParkingGarageMapper parkingGarageMapper;
 
     @Autowired
-    public ParkingGarageServiceImpl(ParkingGarageRepository parkingGarageRepository) {
+    public ParkingGarageServiceImpl(ParkingGarageRepository parkingGarageRepository, ParkingGarageMapper parkingGarageMapper) {
         this.parkingGarageRepository = parkingGarageRepository;
+        this.parkingGarageMapper = parkingGarageMapper;
     }
     @Override
     public void create(ParkingGarageDto parkingGarageDto) {
-
+        parkingGarageRepository.save(parkingGarageMapper.dtoToEntity(parkingGarageDto, new ParkingGarage()));
     }
 
     @Override
     public ParkingGarageDto findById(Long id) {
-        return null;
+        ParkingGarage garage = parkingGarageRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Parking garage with ID \"" + id + "\" not found."));
+        return parkingGarageMapper.entityToDto(garage);
     }
 
     @Override
     public List<ParkingGarageDto> findAll() {
-        return null;
+        return parkingGarageRepository
+                .findAll()
+                .stream()
+                .map(parkingGarageMapper::entityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
+    @Transactional
     public ParkingGarageDto updateById(Long id, ParkingGarageDto parkingGarageDto) {
-        return null;
+        Optional<ParkingGarage> parkingGarageOptional = parkingGarageRepository.findById(id);
+        if (parkingGarageOptional.isPresent()) {
+            ParkingGarage existedCar = parkingGarageOptional.get();
+            ParkingGarage updatedCar = parkingGarageMapper.dtoToEntity(parkingGarageDto, existedCar);
+            return parkingGarageMapper.entityToDto(updatedCar);
+        } else throw new NotFoundException("Parking garage with ID \"" + id + "\" not found.");
     }
 
     @Override
     public void deleteById(Long id) {
-
+        Optional<ParkingGarage> parkingGarageOptional = parkingGarageRepository.findById(id);
+        if (parkingGarageOptional.isPresent()) {
+            parkingGarageRepository.deleteById(id);
+        } else {
+            throw new NotFoundException("Parking garage with ID \"" + id + "\" not found");
+        }
     }
 }
