529d96fc267ced63ad091ff2d42bc02713023151
Artem Prokhorov <electron3djavadev@gmail.com>
2023-09-25 22:07:48 +0300

    wrote few tests for CarServiceImpl

--- src/test/java/com/example/demo/service/CarServiceImplIT.java
+++ src/test/java/com/example/demo/service/CarServiceImplIT.java
@@ -1,19 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.data.entity.Car;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.exception.WrongInputException;
 import com.example.demo.service.impl.CarServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
-@DataJpaTest
-@Sql(scripts = {"classpath:database/create_tables.sql",
-        "classpath:database/populate_tables.sql"},
-        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class CarServiceImplIT {
     @Autowired
     private CarServiceImpl carService;
@@ -21,6 +20,38 @@ public class CarServiceImplIT {
     @Test
     @Transactional
     public void should_return_car() {
-        Car car = carService.findById(1L);
+        Car expectedCar = new Car();
+        expectedCar.setModel("abc");
+        expectedCar.setBrand("123");
+        expectedCar.setParkingStarted(LocalDateTime.now());
+        expectedCar.setLicensePlate("abc123");
+        carService.create(expectedCar);
+
+        Car actualCar = carService.findById(1L);
+
+        Assertions.assertEquals(expectedCar, actualCar);
+    }
+    @Test
+    @Transactional
+    public void should_return_wrong_input_exception() {
+        Car expectedCar = new Car();
+        expectedCar.setModel("abc");
+        expectedCar.setBrand("123");
+        expectedCar.setParkingStarted(LocalDateTime.now());
+        expectedCar.setLicensePlate("abc123");
+        carService.create(expectedCar);
+
+        WrongInputException wrongInputException = Assertions.assertThrows(WrongInputException.class, () -> carService.create(expectedCar));
+        String expectedMessage = "Car with license plate \"abc123\" already exist.";
+        String actualMessage = wrongInputException.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+    @Test
+    @Transactional
+    public void should_return_not_found_exception() {
+        NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> carService.findById(1L));
+        String expectedMessage = "Car with ID \"1\" not found.";
+        String actualMessage = notFoundException.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
     }
 }
